8e1ccdff0f3c51947b886aa9e58d1e4c7a3db8ee
AlexandreNap <alexandre.naprous1@gmail.com>
2022-08-11 11:21:10 +0200

    images cleaning script and randomsubsampling with dates script

---
+++ data/images_folder_cleaning.py
@@ -0,0 +1,53 @@
+import glob
+import os
+import shutil
+from PIL import Image
+import logging
+
+
+def clean_images(path="images\\spots"):
+    logging.info('Started')
+    files = [f for f in glob.glob(f"{path}\\*\\*.jpg", recursive=True) if os.path.getsize(f) > 0]
+    null_files = [f for f in glob.glob("{path}\\*\\*.jpg", recursive=True) if f not in files]
+    logging.debug(len(null_files))
+
+    for f in null_files:
+        os.remove(f)
+
+    size_files = [os.path.getsize(f) for f in files]
+
+    # get images having exactly the same size then previous image (high chance of being the same image)
+    duplicated_idx = []
+    previous_size = -1
+    for i, size in enumerate(size_files):
+        if size == previous_size:
+            duplicated_idx.append(i)
+        previous_size = size
+
+    logging.debug(duplicated_idx)
+
+    logging.info("Removing duplicates ...")
+    logging.debug(len(files))
+    for idx in sorted(duplicated_idx, reverse=True):
+        os.remove(files[idx])
+        del files[idx]
+    logging.debug(len(files))
+
+    logging.info("verifying each images and removing bad ones...")
+    for filename in files:
+        if filename.endswith('.jpg'):
+            try:
+                img = Image.open(filename)  # open the image file
+                img.verify()  # verify that it is, in fact an image
+            except (IOError, SyntaxError) as e:
+                print(filename)
+                os.remove(filename)
+
+    logging.info("Zipping files")
+    shutil.make_archive("images\\spots", 'zip', "images\\spots\\")
+    logging.info("Finished")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(filename='myapp.log', level=logging.INFO)
+    clean_images()
---
+++ data/random_images_between_dates.py
@@ -0,0 +1,29 @@
+import glob
+import os
+import random
+import pandas as pd
+import shutil
+import re
+
+
+def subsample_between_dates(date_min, date_max, n_max=200, path="images\\spots"):
+    orig_files = [f for f in glob.glob(f"{path}\\*\\*.jpg", recursive=True) if os.path.getsize(f) > 0]
+    files = [f.replace(f"{path}\\", "").replace("\\", "__") for f in orig_files]
+    directory = f"subsample_from_{date_min}_to_{date_max}"
+    data = pd.DataFrame({"spot": [re.sub("__.*", "", f) for f in files],
+                         "date": [re.sub(".*__", "", f).replace(".jpg", "") for f in files],
+                         "file_path": orig_files,
+                         "new_path": [f"{directory}\\{f}" for f in files]})
+    data['date'] = pd.to_datetime(data.date, format='%Y-%m-%d_%H-%M')
+    data = data[(date_min < data.date) & (data.date < date_max)]
+    if len(data) > n_max:
+        data = data.sample(n_max)
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    for path, new_path in zip(data.file_path, data.new_path):
+        shutil.copyfile(path, new_path)
+
+
+if __name__ == "__main__":
+    subsample_between_dates("2022-06-01", "2022-07-15")
